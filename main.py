fba8db706df6d697364a8053564e636644facb95
haolam07 <57072114+haolam07@users.noreply.github.com>
2022-06-21 21:55:45 +0000

    if statement

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
-isHungry = True
-isBored = False
+isHungry = input("Are you hungry?(y/n) ")
+isBored = input("Are you bored?(y/n) ")
 
 
-if (isHungry == True):
+if (isHungry == "y" or isHungry == "Y"):
   print("Go eat")
 else:
   print("Don't eat")
 
-if (isBored == True):
-  print("Go read")
+if (isBored == "y" or isBored == "Y"):
+  print("Go to sleep")
 else:
-  print("Go exercise")
+  print("Go exercise") 
+
+userNumber = int(input("Enter a whole number: "))
+
+if (userNumber >=0):
+  print("Your number is a positive number")
+else:
+  print("Your number is a negative number")
+
+userAge = int(input("Enter you age: ")) 
+
+# asks the user for their age and tell the user what movie they can watch based on their age
+if (userAge > 17):
+  print("You can watch any movie")
+elif (userAge < 17 and userAge >13):
+  print("You can watch g-rated and pg-13 movies")
+else:
+  print("you can only watch g-rated movies")
