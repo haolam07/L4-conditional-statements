952a87c059a046ed2c490dc85dc74217b127a412
haolam07 <57072114+haolam07@users.noreply.github.com>
2022-06-22 16:40:55 +0000

    quadrant if statement

--- README.md
+++ README.md
@@ -12,3 +12,21 @@ else:
   Body statement
 ```
 python relies on indentation to define scope
+
+### Elif
+`elif` allows us to add more than one condition to our `if` blocks
+
+Conditional statements always start with an `if` header, `elif` is optional, but we can have as many as we want. `else` is also optional, but we can only have 1 `else` for every `if`
+
+Ex.
+``` python
+month = 2
+if (month == 1):
+  print("January")
+elif (month == 2:
+  print("february")
+elif (month == 3):
+  print("March")
+else:
+  print("The month is betwee April and December")
+```
--- main.py
+++ main.py
@@ -19,12 +19,32 @@ if (userNumber >=0):
 else:
   print("Your number is a negative number")
 
-userAge = int(input("Enter you age: ")) 
+userAge = int(input("Enter your age: ")) 
 
 # asks the user for their age and tell the user what movie they can watch based on their age
-if (userAge > 17):
+if (userAge >=17):
   print("You can watch any movie")
-elif (userAge < 17 and userAge >13):
+elif (userAge >= 13):
   print("You can watch g-rated and pg-13 movies")
 else:
   print("you can only watch g-rated movies")
+
+#asks the user for an x and a y value
+#base on the x and y value, output which quadrant they're in
+
+userX = int(input("Enter x value: "))
+userY = int(input("Enter y value: "))
+if (userX > 0 and userY > 0):
+  print("You're in quadrant 1")
+elif ( userX < 0 and userY < 0):
+  print("You're in quadrant 3")
+elif (userX < 0 and userY > 0):
+  print("You're in quadrant 2")
+elif (userX > 0 and userY < 0):
+  print("You're in quadrant 4")
+elif (userX == 0 and userY == 0):
+  print("You're on the origin")
+elif (userY == 0):
+  print("you're on the x axis")
+else:
+  print("you're on the y axis")
